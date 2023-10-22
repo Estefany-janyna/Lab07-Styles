7d0014a0efca5947ae44fcec77bf8ba942766c59
Estefany-janyna <estefany.taipe@tecsup.edu.pe>
2023-10-21 23:49:13 -0500

    Lab07-Camb.

--- Lab07/Lab07/MainPage.xaml.cs
+++ Lab07/Lab07/MainPage.xaml.cs
@@ -13,6 +13,34 @@ namespace Lab07
         public MainPage()
         {
             InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new globaldemo());
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
         }
     }
 }
